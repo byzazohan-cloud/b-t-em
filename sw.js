d823fbe7c39829ec30a54323e7f944164c05c4d1
byzazohan-cloud <byzazohan@gmail.com>
2026-09-12 19:50:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,1 @@
-const CACHE='butcem-premium-secure-v5';
-const ASSETS=['./','./index.html','./styles.css?v=5','./app.js?v=5','./manifest.json','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  const url=new URL(e.request.url);
-  if(url.origin!==self.location.origin) return;
-  const isNavigation=e.request.mode==='navigate';
-  if(isNavigation){
-    e.respondWith(fetch(e.request).then(resp=>{if(resp.ok)caches.open(CACHE).then(c=>c.put('./index.html',resp.clone()));return resp}).catch(()=>caches.match('./index.html')));
-    return;
-  }
-  e.respondWith(fetch(e.request).then(resp=>{if(resp.ok)caches.open(CACHE).then(c=>c.put(e.request,resp.clone()));return resp}).catch(()=>caches.match(e.request)));
-});
+const C='hane-premium-v1',A=['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(xs=>Promise.all(xs.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
