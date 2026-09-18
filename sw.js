65e2e943764197abb8699d619a5126204c4ba367
byzazohan-cloud <byzazohan@gmail.com>
2026-09-18 21:04:26 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* HANE PWA update worker - network first for app shell */
-const CACHE_NAME = 'hane-v19-4-8-g2-month-20260918';
+const CACHE_NAME = 'hane-v19-4-8-g2-monthfix1-20260918';
 const APP_SHELL = [
   './', './index.html', './styles.css', './app-v19.js', './manifest.json',
   './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'
