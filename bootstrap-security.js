70527164b68ed41417c76ecaa7bc54a307054be3
byzazohan-cloud <byzazohan@gmail.com>
2026-09-20 18:38:32 +0300

    Add files via upload

--- bootstrap-security.js
+++ bootstrap-security.js
@@ -1,6 +1,6 @@
 'use strict';
 (() => {
-  const BUILD='19.4.8.20260920-LOCAL-DATA-ONLY-50-THEME-COLOR-BINDING';
+  const BUILD='19.4.8.20260920-LOCAL-DATA-ONLY-51-THEME-TRUE-BINDING';
   const KEY='hane_app_shell_build';
   try {
     if (localStorage.getItem(KEY) !== BUILD) {
