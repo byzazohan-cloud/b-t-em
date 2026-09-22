ace23b6d5caf2a3c2a665730cfe938afe0ea4c92
byzazohan-cloud <byzazohan@gmail.com>
2026-09-22 18:45:24 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,8 +7,8 @@
       engine file is extracted/cached.
    4) Runtime is cache-only: no page/worker request is allowed to reach the network.
 */
-const SW_BUILD = '19.4.8.20260922-V113-PLUS-S1';
-const CACHE_NAME = 'hane-v19-4-8-V113-PLUS-S1';
+const SW_BUILD = '19.4.8.20260921-V113-VIEW-FUNCTIONS-RESTORE';
+const CACHE_NAME = 'hane-v19-4-8-V113-VIEW-FUNCTIONS-RESTORE';
 const HANE_CACHE_PREFIX = 'hane-';
 
 
