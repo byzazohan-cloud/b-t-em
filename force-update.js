2facb05f91d2fa29fb982f63bf9ce0216c6876fa
byzazohan-cloud <byzazohan@gmail.com>
2026-09-23 19:04:15 +0300

    Add files via upload

--- force-update.js
+++ force-update.js
@@ -13,5 +13,5 @@
       await Promise.all(ks.filter(k=>k.startsWith('hane-')).map(k=>caches.delete(k)));
     }
   }catch(e){}
-  location.replace('./?v=19.4.19-V113-S16-AUDIT-FIX&fresh='+Date.now());
+  location.replace('./?v=19.4.20-V113-S17-EKSTRE2&fresh='+Date.now());
 })();
