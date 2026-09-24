fba5ad036970a20d7bbe9c8c6065aab947f63890
byzazohan-cloud <byzazohan@gmail.com>
2026-09-24 19:04:30 +0300

    Add files via upload

--- force-update.js
+++ force-update.js
@@ -13,5 +13,5 @@
       await Promise.all(ks.filter(k=>k.startsWith('hane-')).map(k=>caches.delete(k)));
     }
   }catch(e){}
-  location.replace('./?v=19.4.36-V113-S19-HAN-FILTER1&fresh='+Date.now());
+  location.replace('./?v=19.4.37-V113-S19-HAN-DIKKAT1&fresh='+Date.now());
 })();
